0b412b02e7b8b7ff7c970e58faf95b3c3069a19f
jawaldi348 <jawaldi416@gmail.com>
2021-03-04 17:08:47 +0700

    defenisikan beberapa variabel dan pemanggilan database

--- database/ChatUser.php
+++ database/ChatUser.php
@@ -4,4 +4,21 @@
 
 class ChatUser
 {
+    private $user_id;
+    private $user_name;
+    private $user_email;
+    private $user_password;
+    private $user_profile;
+    private $user_status;
+    private $user_created_on;
+    private $user_verification_code;
+    private $user_login_status;
+    public $connect;
+
+    public function __construct()
+    {
+        require_once('Database_connection.php');
+        $database_object = new Database_connection;
+        $this->connect = $database_object->connect();
+    }
 }
