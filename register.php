9f5c056c82c6ac27a1576d0e9cc5e2bc8134fcff
jawaldi348 <jawaldi416@gmail.com>
2021-03-04 16:55:08 +0700

    halaman register dan validasi form input

---
+++ register.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Register | PHP Chat Application using Websocket</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor-front/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <link href="vendor-front/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" type="text/css" href="vendor-front/parsley/parsley.css" />
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor-front/jquery/jquery.min.js"></script>
+    <script src="vendor-front/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor-front/jquery-easing/jquery.easing.min.js"></script>
+
+    <script type="text/javascript" src="vendor-front/parsley/dist/parsley.min.js"></script>
+</head>
+
+<body>
+    <div class="containter">
+        <br />
+        <br />
+        <h1 class="text-center">PHP Chat Application using Websocket &mdash; Register</h1>
+
+        <div class="row justify-content-md-center">
+            <div class="col col-md-4 mt-5">
+                <div class="card">
+                    <div class="card-header">Register</div>
+                    <div class="card-body">
+                        <form method="post" id="register_form">
+                            <div class="form-group">
+                                <label>Enter Your Name</label>
+                                <input type="text" name="user_name" id="user_name" class="form-control" data-parsley-pattern="/^[a-zA-Z\s]+$/" required />
+                            </div>
+                            <div class="form-group">
+                                <label>Enter Your Email</label>
+                                <input type="text" name="user_email" id="user_email" class="form-control" data-parsley-type="email" required />
+                            </div>
+                            <div class="form-group">
+                                <label>Enter Your Password</label>
+                                <input type="password" name="user_password" id="user_password" class="form-control" data-parsley-minlength="6" data-parsley-maxlength="12" data-parsley-pattern="^[a-zA-Z]+$" required />
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" name="register" class="btn btn-success" value="Register" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
+
+<script>
+    $(document).ready(function() {
+        $('#register_form').parsley();
+    });
+</script>
