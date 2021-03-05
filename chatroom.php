47e6bc3a9bcbd9e09e0e44883e585d7496f4fe39
jawaldi348 <jawaldi416@gmail.com>
2021-03-05 22:17:29 +0700

    halaman chatroom jika sudah login

---
+++ chatroom.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Chat application in php using web scocket programming</title>
+    <!-- Bootstrap core CSS -->
+    <link href="vendor-front/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <link href="vendor-front/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" type="text/css" href="vendor-front/parsley/parsley.css" />
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor-front/jquery/jquery.min.js"></script>
+    <script src="vendor-front/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor-front/jquery-easing/jquery.easing.min.js"></script>
+
+    <script type="text/javascript" src="vendor-front/parsley/dist/parsley.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <br />
+        <h3 class="text-center">Welcome to Chatroom</h3>
+    </div>
+</body>
+
+</html>
